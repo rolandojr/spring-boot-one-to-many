1d24f82f50483364b207a47cb1c8683d5ff16b34
Rolando Javier <rolandojavier.ramos@gmail.com>
2024-03-22 20:16:13 -0500

    add mysql-connection

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tutorials (title, description, published) VALUES (1, 'Tuto#1', 'Tuto#1Description', true);
-INSERT INTO tutorials (title, description, published) VALUES (2, 'Tuto#2', 'Tuto#2Description', true);
-INSERT INTO tutorials (title, description, published) VALUES (3, 'Tuto#3', 'Tuto#3Description', false);
+INSERT INTO tutorials (title, description, published) VALUES ('Tuto#1', 'Tuto#1Description', true);
+INSERT INTO tutorials (title, description, published) VALUES ('Tuto#2', 'Tuto#2Description', true);
+INSERT INTO tutorials (title, description, published) VALUES ('Tuto#3', 'Tuto#3Description', false);
